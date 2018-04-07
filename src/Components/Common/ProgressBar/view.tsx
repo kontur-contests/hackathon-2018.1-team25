dce2dfabf2ca0c0c5506c1e55fd76a0c02961a15
dgal <dgal@skbkontur.ru>
2018-04-07 21:38:33 +0500

    Add progress bar

---
+++ src/Components/Common/ProgressBar/view.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as styles from './style.pcss';
+
+export type ProgressBarProps = {
+    now: number,
+    max: number,
+    frontColor: string,
+    backColor: string,
+}
+
+export default class ProgressBar extends React.Component<ProgressBarProps> {
+    render() {
+        const {now, max, frontColor, backColor} = this.props;
+
+        return (
+            <div className={styles.Bar}>
+                <div className={styles.Bar__progressWrapper} style={{
+                    borderColor: frontColor,
+                    backgroundColor: backColor,
+                }}>
+                    {now > 0 && max &&
+                        <div className={styles.Bar__progress} style={{
+                            width: now / max * 100 + '%',
+                            backgroundColor: frontColor,
+                        }}/>
+                    }
+                    <div className={styles.Bar__title}>{now} / {max}</div>
+                </div>
+            </div>
+        )
+    }
+}
